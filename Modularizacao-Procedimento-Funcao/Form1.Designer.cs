6b13002a471cbc098774370340e2e6705bd650eb
jpbertti <joaobertty@gmail.com>
2024-12-03 10:49:09 -0300

    Adicionar arquivos de projeto.

---
+++ Modularizacao-Procedimento-Funcao/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Modularizacao_Procedimento_Funcao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.gpbOperadores = new System.Windows.Forms.GroupBox();
+            this.rdbResto = new System.Windows.Forms.RadioButton();
+            this.rdbDivisao = new System.Windows.Forms.RadioButton();
+            this.rdbMultiplicacao = new System.Windows.Forms.RadioButton();
+            this.rdbSubtracao = new System.Windows.Forms.RadioButton();
+            this.rdbSoma = new System.Windows.Forms.RadioButton();
+            this.lblNumero1 = new System.Windows.Forms.Label();
+            this.lblNumero2 = new System.Windows.Forms.Label();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.txtExpressao = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gpbOperadores.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(359, 324);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(98, 58);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(75, 324);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(98, 58);
+            this.btnSair.TabIndex = 1;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(218, 324);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(98, 58);
+            this.btnLimpar.TabIndex = 2;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // gpbOperadores
+            // 
+            this.gpbOperadores.Controls.Add(this.rdbResto);
+            this.gpbOperadores.Controls.Add(this.rdbDivisao);
+            this.gpbOperadores.Controls.Add(this.rdbMultiplicacao);
+            this.gpbOperadores.Controls.Add(this.rdbSubtracao);
+            this.gpbOperadores.Controls.Add(this.rdbSoma);
+            this.gpbOperadores.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gpbOperadores.Location = new System.Drawing.Point(127, 64);
+            this.gpbOperadores.Name = "gpbOperadores";
+            this.gpbOperadores.Size = new System.Drawing.Size(253, 154);
+            this.gpbOperadores.TabIndex = 3;
+            this.gpbOperadores.TabStop = false;
+            this.gpbOperadores.Text = "Selecione o Operador";
+            // 
+            // rdbResto
+            // 
+            this.rdbResto.AutoSize = true;
+            this.rdbResto.Location = new System.Drawing.Point(7, 122);
+            this.rdbResto.Name = "rdbResto";
+            this.rdbResto.Size = new System.Drawing.Size(117, 24);
+            this.rdbResto.TabIndex = 4;
+            this.rdbResto.TabStop = true;
+            this.rdbResto.Text = "Resto ( % )";
+            this.rdbResto.UseVisualStyleBackColor = true;
+            // 
+            // rdbDivisao
+            // 
+            this.rdbDivisao.AutoSize = true;
+            this.rdbDivisao.Location = new System.Drawing.Point(7, 99);
+            this.rdbDivisao.Name = "rdbDivisao";
+            this.rdbDivisao.Size = new System.Drawing.Size(117, 24);
+            this.rdbDivisao.TabIndex = 3;
+            this.rdbDivisao.TabStop = true;
+            this.rdbDivisao.Text = "Divisão ( / )";
+            this.rdbDivisao.UseVisualStyleBackColor = true;
+            // 
+            // rdbMultiplicacao
+            // 
+            this.rdbMultiplicacao.AutoSize = true;
+            this.rdbMultiplicacao.Location = new System.Drawing.Point(7, 76);
+            this.rdbMultiplicacao.Name = "rdbMultiplicacao";
+            this.rdbMultiplicacao.Size = new System.Drawing.Size(166, 24);
+            this.rdbMultiplicacao.TabIndex = 2;
+            this.rdbMultiplicacao.TabStop = true;
+            this.rdbMultiplicacao.Text = "Multiplicação ( x )";
+            this.rdbMultiplicacao.UseVisualStyleBackColor = true;
+            // 
+            // rdbSubtracao
+            // 
+            this.rdbSubtracao.AutoSize = true;
+            this.rdbSubtracao.Location = new System.Drawing.Point(7, 53);
+            this.rdbSubtracao.Name = "rdbSubtracao";
+            this.rdbSubtracao.Size = new System.Drawing.Size(143, 24);
+            this.rdbSubtracao.TabIndex = 1;
+            this.rdbSubtracao.TabStop = true;
+            this.rdbSubtracao.Text = "Subtração ( - )";
+            this.rdbSubtracao.UseVisualStyleBackColor = true;
+            // 
+            // rdbSoma
+            // 
+            this.rdbSoma.AutoSize = true;
+            this.rdbSoma.Location = new System.Drawing.Point(7, 30);
+            this.rdbSoma.Name = "rdbSoma";
+            this.rdbSoma.Size = new System.Drawing.Size(110, 24);
+            this.rdbSoma.TabIndex = 0;
+            this.rdbSoma.TabStop = true;
+            this.rdbSoma.Text = "Soma ( + )";
+            this.rdbSoma.UseVisualStyleBackColor = true;
+            // 
+            // lblNumero1
+            // 
+            this.lblNumero1.AutoSize = true;
+            this.lblNumero1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero1.Location = new System.Drawing.Point(18, 25);
+            this.lblNumero1.Name = "lblNumero1";
+            this.lblNumero1.Size = new System.Drawing.Size(96, 20);
+            this.lblNumero1.TabIndex = 4;
+            this.lblNumero1.Text = "Numero 1: ";
+            // 
+            // lblNumero2
+            // 
+            this.lblNumero2.AutoSize = true;
+            this.lblNumero2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero2.Location = new System.Drawing.Point(284, 25);
+            this.lblNumero2.Name = "lblNumero2";
+            this.lblNumero2.Size = new System.Drawing.Size(96, 20);
+            this.lblNumero2.TabIndex = 5;
+            this.lblNumero2.Text = "Numero 2: ";
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(121, 24);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero1.TabIndex = 6;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(386, 24);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero2.TabIndex = 7;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(121, 249);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(100, 20);
+            this.txtResultado.TabIndex = 8;
+            // 
+            // txtExpressao
+            // 
+            this.txtExpressao.Location = new System.Drawing.Point(386, 251);
+            this.txtExpressao.Name = "txtExpressao";
+            this.txtExpressao.Size = new System.Drawing.Size(100, 20);
+            this.txtExpressao.TabIndex = 9;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(19, 249);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(96, 20);
+            this.lblResultado.TabIndex = 10;
+            this.lblResultado.Text = "Resultado:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(277, 251);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Expressão: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 404);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtExpressao);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.lblNumero2);
+            this.Controls.Add(this.lblNumero1);
+            this.Controls.Add(this.gpbOperadores);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnCalcular);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.gpbOperadores.ResumeLayout(false);
+            this.gpbOperadores.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.GroupBox gpbOperadores;
+        private System.Windows.Forms.RadioButton rdbDivisao;
+        private System.Windows.Forms.RadioButton rdbMultiplicacao;
+        private System.Windows.Forms.RadioButton rdbSubtracao;
+        private System.Windows.Forms.RadioButton rdbSoma;
+        private System.Windows.Forms.RadioButton rdbResto;
+        private System.Windows.Forms.Label lblNumero1;
+        private System.Windows.Forms.Label lblNumero2;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.TextBox txtExpressao;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Modularizacao-Procedimento-Funcao/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Modularizacao_Procedimento_Funcao
+{
+
+    public partial class Form1 : Form
+    {
+        //Declaração de variaveis globais
+        double resultado = 0;
+        string expressao = "";
+
+        // Função Soma
+        void Soma(double num1, double num2)
+        {
+            resultado = num1 + num2;
+            expressao = num1 + " + " + num2;
+        }
+
+        // Função Subtração
+        void Subtracao(double num1, double num2)
+        {
+            resultado = num1 - num2;
+            expressao = num1 + " - " + num2;
+        }
+
+        // Função Multiplicação
+        void Multiplicacao(double num1, double num2)
+        {
+            resultado = num1 * num2;
+            expressao = num1 + " x " + num2;
+        }
+
+        // Função Divisão
+        void Divisao(double num1, double num2)
+        {
+            resultado = num1 / num2;
+            expressao = num1 + " / " + num2;
+        }
+
+        // Função Resto
+        void Resto(double num1, double num2)
+        {
+            resultado = num1 % num2;
+            expressao = num1 + " % " + num2;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNumero1.Clear();
+            txtNumero2.Clear();
+            txtExpressao.Clear();
+            txtResultado.Clear();
+            txtNumero1.Focus();
+            rdbSoma.Checked = false;
+            rdbSubtracao.Checked = false;
+            rdbDivisao.Checked = false;
+            rdbMultiplicacao.Checked = false;
+            rdbResto.Checked = false;
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja realmente sair?", "Sair", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Close();
+            }
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double num1 = double.Parse(txtNumero1.Text);
+
+            double num2 = double.Parse(txtNumero2.Text);
+
+            if (rdbSoma.Checked == true)
+
+            {
+
+                Soma(num1, num2);
+
+            }
+
+            else if (rdbSubtracao.Checked == true)
+
+            {
+
+                Subtracao(num1, num2);
+
+            }
+
+            else if (rdbMultiplicacao.Checked == true)
+
+            {
+
+                Multiplicacao(num1, num2);
+
+            }
+
+            else if (rdbDivisao.Checked == true)
+
+            {
+
+                Divisao(num1, num2);
+
+            }
+
+            else if (rdbResto.Checked == true)
+
+            {
+
+                Resto(num1, num2);
+
+            }
+
+
+            txtResultado.Text = resultado.ToString();
+
+            txtExpressao.Text = expressao;
+
+        }
+    }
+}
